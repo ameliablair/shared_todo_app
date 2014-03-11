522ce0ebe9b99b8a24e9483d08227b46265ac27b
ameliablair <ameliablair@gmail.com>
2014-03-10 20:37:27 -0400

    added app folders and uncommented out the racer gem

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+SharedTodoApp::Application.config.secret_key_base = '1328813f938c49447ab241523270ac62b21d01d67fa38da9a0c56866be10de2f5cf9a1a08899f45c7ec6bb5892c9c7a00a2f34bda6eb7264d4da20cc9b167775'
